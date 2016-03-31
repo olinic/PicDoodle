470d4cf49d7c5d2ad9d3120362c5ba0a781d2670
Oliver <oliver.nic012@gmail.com>
2016-03-31 15:42:21 -0400

    Output and header to email

--- sendEmail.php
+++ sendEmail.php
@@ -3,6 +3,7 @@
   Checks the database to determine if we need to send an email.
   If so, send an email with the link to the login page.
 
+
   To periodically check, add the following to a cron job:*/
 //        * * */5 * * /usr/local/bin/php -q /www/scripts/myscript.php
 
@@ -20,12 +21,13 @@
     $message = "Thank you for participating in the Doodle Experiment! \r\n"
                   . "Please follow this link to login with your doodle and password: \r\n"
                   . "<a href='mydoodle.duckdns.org/login.php'>Login</a> \r\n \r\n";
-    $success = mail($to, $subject, $message);
+    $headers = "From: do-not-reply@mydoodle.duckdns.org";
+    $success = mail($to, $subject, $message, $headers);
 
     if ($success) {
-      echo "Successfully sent email<br>"
+      echo "Successfully sent email<br>";
     } else {
-      echo "Failed to send email<br>"
+      echo "Failed to send email<br>";
     }
   }
 
